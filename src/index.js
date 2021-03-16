8f2f30ce0471f73e7b654e48ec28b3333f74b64d
manhloi0505@gmail.com <manhloi0505@gmail.com>
2021-03-16 23:25:11 +0700

    Refactor code: create routers, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 4000;
 
+const route = require("./routes");
+
 // http logger
 // app.use(morgan("combined"));
 
@@ -28,22 +30,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log("Form data", req.body);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
